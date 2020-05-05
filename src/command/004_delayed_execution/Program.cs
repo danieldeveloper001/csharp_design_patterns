34b85e2f4b65a9e7d5d4e729d5fdde616aef8df5
Daniel Augusto <danieldeveloper001@gmail.com>
2020-05-04 23:13:28 -0300

    adds delayed execution for the command pattern

---
+++ src/command/004_delayed_execution/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var gameElement = new GameElement(0, 0);
+            var gameTurn = new GameTurn();
+
+            gameTurn.Add(new MovementAction(gameElement, 10, 0));
+            gameTurn.Add(new MovementAction(gameElement, 0, 10));
+            gameTurn.Add(new MovementAction(gameElement, -20, 0));
+            gameTurn.Add(new MovementAction(gameElement, 0, -20));
+            gameTurn.Add(new MovementAction(gameElement, 20, 0));
+            gameTurn.Add(new MovementAction(gameElement, 0, 10));
+            gameTurn.Add(new MovementAction(gameElement, -10, 0));
+
+            gameTurn.Execute();
+        }
+    }
+}
