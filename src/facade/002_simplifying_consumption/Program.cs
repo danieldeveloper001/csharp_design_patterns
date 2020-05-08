ff9e65cb894c6440f30e49ab5060eb2e3e283d9c
Daniel Augusto <danieldeveloper001@gmail.com>
2020-05-07 21:26:26 -0300

    :construction: Adds empty project for the facade simplifying consumption example.

---
+++ src/facade/002_simplifying_consumption/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using Project.Model;
+using Project.Repository;
+
+namespace Project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
