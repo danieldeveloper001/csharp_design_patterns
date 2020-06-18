e4e82c51de9519a20bf74688d7d4e70137576b8b
Daniel Augusto <danieldeveloper001@gmail.com>
2020-06-17 22:21:23 -0300

    :construction: Work in progress on the example interactivity.

--- src/flyweight/002_concrete/Program.cs
+++ src/flyweight/002_concrete/Program.cs
@@ -9,27 +9,68 @@ namespace Project
         {
             var mapBlockFactory = new MapBlockFactory();
 
-            using (var reader = new StreamReader("./Maps/Level1.txt"))
+            //TODO: REFACTOR
+            var playerX = 0;
+            var playerY = 0;
+
+            Console.Clear();
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Console.CursorVisible = false;
+
+            do
             {
-                Console.Clear();
-
-                var rowIndex = 0;
-                while (reader.ReadLine() is string row)
+                //TODO: IN MEMORY MAP
+                using (var reader = new StreamReader("./Maps/Level1.txt"))
                 {
-                    var columnIndex = 0;
-                    while(columnIndex < row.Length && row[columnIndex] is char column)
+                    var rowIndex = 0;
+                    while (reader.ReadLine() is string row)
                     {
-                        var block = mapBlockFactory.GetMapBlock(column);
-                        block.Draw(columnIndex, rowIndex);
+                        var columnIndex = 0;
+                        while(columnIndex < row.Length && row[columnIndex] is char column)
+                        {
+                            var block = mapBlockFactory.GetMapBlock(column);
+                            block.Draw(columnIndex, rowIndex);
 
-                        columnIndex++;
+                            columnIndex++;
+                        }
+
+                        rowIndex++;
                     }
-
-                    rowIndex++;
                 }
 
-                Console.WriteLine();
-            }
+                //TODO: REFACTOR
+                Console.SetCursorPosition(playerX, playerY);
+                Console.ForegroundColor = ConsoleColor.Black;
+                Console.Write("\u263A");
+
+                var key = Console.ReadKey();
+                switch(key.Key)
+                {
+                    case ConsoleKey.UpArrow:
+                        if(playerY > 0)
+                            playerY--;
+                        break;
+                    case ConsoleKey.DownArrow:
+                        if(playerY < 25)
+                            playerY++;
+                        break;
+                    case ConsoleKey.LeftArrow:
+                        if(playerX > 0)
+                            playerX--;
+                        break;
+                    case ConsoleKey.RightArrow:
+                        if(playerX < 50)
+                            playerX++;
+                        break;
+                    case ConsoleKey.Escape:
+                        Console.Clear();
+                        Console.CursorVisible = true;
+                        Environment.Exit(0);
+                        break;
+                    default:
+                        break;
+                }
+            } while(true);
         }
     }
 }
