8fa57815d474f30a7d368ebeabbb815ea586c346
Daniel Augusto <danieldeveloper001@gmail.com>
2020-06-20 14:08:16 -0300

    :construction: Refactors game loop into hud and game classes.

--- src/flyweight/002_concrete/Program.cs
+++ src/flyweight/002_concrete/Program.cs
@@ -1,56 +1,132 @@
 ﻿using System;
-using System.IO;
 
 namespace Project
 {
+
+    class Hud
+    {
+        private int _gameColumns;
+        private int _gameRows;
+
+        public Hud(int gameColumns, int gameRows)
+        {
+            _gameColumns = gameColumns;
+            _gameRows = gameRows;
+        }
+
+        public void Draw(string status)
+        {
+            Console.ForegroundColor = ConsoleColor.White;
+
+            Console.SetCursorPosition(0, _gameRows + 1);
+            Console.Write('+');
+            Console.Write(new string('-', _gameColumns - 2));
+            Console.Write('+');
+
+            Console.SetCursorPosition(0, _gameRows + 2);
+            Console.WriteLine($"| {status, 46} |"); //TODO: _gameColumns - 4
+
+            Console.SetCursorPosition(0, _gameRows + 3);
+            Console.Write('+');
+            Console.Write(new string('-', _gameColumns - 2));
+            Console.Write('+');
+
+            Console.SetCursorPosition(0, Settings.MAP_ROWS + 1);
+
+            Console.Write(new string('-', _gameColumns - 2));
+
+        }
+    }
+
+    class Game
+    {
+        private Action _onStartAction;
+        private Action _onEndAction;
+        private Hud _hud;
+        private Map _map;
+        private Player _player;
+
+        public Game(
+            Action onStartAction,
+            Action onEndAction,
+            Hud hud,
+            Map map,
+            Player player)
+        {
+            _onStartAction = onStartAction;
+            _onEndAction = onEndAction;
+            _hud = hud;
+            _map = map;
+            _player = player;
+        }
+
+        public void Start()
+        {
+            _onStartAction();
+
+            do
+            {
+                _map.Draw();
+                _player.Draw();
+
+                //TODO: MAKE THE HUD LISTEN TO CHANGES ON THE PLAYER POSITION
+                var currentBlock = _map.GetBlockAt(_player.Column, _player.Row);
+                var status = $"Player is currently at a {currentBlock.Description}";
+                _hud.Draw(status);
+
+                ReadNextAction();
+
+            } while(true);
+
+            // _onEndAction();
+        }
+
+        private void ReadNextAction()
+        {
+            //TODO: MAKE GAME UNAWARE OF THE CONSOLE
+            var key = Console.ReadKey();
+            switch(key.Key)
+            {
+                case ConsoleKey.UpArrow:
+                    _player.MoveUp();
+                    break;
+                case ConsoleKey.DownArrow:
+                    _player.MoveDown();
+                    break;
+                case ConsoleKey.LeftArrow:
+                    _player.MoveLeft();
+                    break;
+                case ConsoleKey.RightArrow:
+                    _player.MoveRight();
+                    break;
+                case ConsoleKey.Escape:
+                    _onEndAction();
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+
     class Program
     {
-
         static void Main(string[] args)
         {
-            ConsoleSetup();
+            var hud = new Hud(Settings.MAP_COLUMNS, Settings.MAP_ROWS);
 
             var map = new Map(new MapBlockFactory(), Settings.MAP_COLUMNS, Settings.MAP_ROWS);
             map.Load(Settings.MAP_PATH);
 
             var player = new Player();
 
-            do
-            {
-                map.Draw();
-                player.Draw();
+            var game = new Game(
+                onStartAction: ConsoleSetup,
+                onEndAction: ConsoleTeardown,
+                hud: hud,
+                map: map,
+                player: player);
 
-                var currentBlock = map.GetBlockAt(player.Column, player.Row);
-
-                var message = $"Player is currently at a {currentBlock.Description}";
-                Console.ForegroundColor = ConsoleColor.White;
-                Console.SetCursorPosition(0, Settings.MAP_ROWS + 1);
-                Console.WriteLine($"\n{message, 50}");
-
-
-                var key = Console.ReadKey();
-                switch(key.Key)
-                {
-                    case ConsoleKey.UpArrow:
-                        player.MoveUp();
-                        break;
-                    case ConsoleKey.DownArrow:
-                        player.MoveDown();
-                        break;
-                    case ConsoleKey.LeftArrow:
-                        player.MoveLeft();
-                        break;
-                    case ConsoleKey.RightArrow:
-                        player.MoveRight();
-                        break;
-                    case ConsoleKey.Escape:
-                        ConsoleTeardown();
-                        Environment.Exit(0);
-                        break;
-                    default:
-                        break;
-                }
-            } while(true);
+            game.Start();
         }
 
         private static void ConsoleSetup()
@@ -67,4 +143,5 @@ namespace Project
         }
     }
 }
+
 
