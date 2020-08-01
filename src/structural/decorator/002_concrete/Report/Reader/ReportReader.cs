ca9963de50469cfd179c02c02c1570c19760f180
Daniel Augusto <danieldeveloper001@gmail.com>
2020-07-31 22:26:03 -0300

    :sparkles: adds concrete decorator example.

---
+++ src/structural/decorator/002_concrete/Report/Reader/ReportReader.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Project
+{
+    class ReportReader : IReportReader
+    {
+        public ReportReader() { }
+
+        public IEnumerable<Report> Read(string path)
+        {
+            var report = new List<Report>();
+
+            using (StreamReader reader = new StreamReader(path))
+            {
+                reader.ReadLine();
+                while(!reader.EndOfStream)
+                {
+                    var line = reader.ReadLine().Split(';');
+                    report.Add(
+                        new Report()
+                        {
+                            Id = Guid.Parse(line[0]),
+                            Product = line[1],
+                            Amount = Int32.Parse(line[2]),
+                            Value = Decimal.Parse(line[3])
+                        });
+                }
+            }
+
+            return report;
+        }
+    }
+}
---
+++ src/structural/decorator/002_concrete/Report/Reader/ReportReaderLogger.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Project
+{
+    class ReportReaderLogger : IReportReader
+    {
+        private IReportReader _reportReader;
+
+        public ReportReaderLogger(IReportReader ReportReader)
+        {
+            _reportReader = ReportReader;
+        }
+
+        public IEnumerable<Report> Read(string path)
+        {
+            Console.WriteLine($"[{DateTime.Now}] Reading report file...");
+            var report = _reportReader.Read(path);
+            Console.WriteLine($"[{DateTime.Now}] Done.");
+
+            return report;
+        }
+    }
+}
